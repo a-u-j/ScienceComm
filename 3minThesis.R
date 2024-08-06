51181978aa67484810fb3c03acd40a8236faa6f6
a-u-j <amelia.jeffery@unsw.edu.au>
2024-08-06 16:12:25 +1000

    Update 3minThesis.R

--- 3minThesis.R
+++ 3minThesis.R
@@ -1,2 +1,8 @@
+install.packages("remotes")
+library(remotes)
+install_version("moveVis", "0.10.0")
+library(moveVis)
 
 data <- read.csv("~/GitHub/collar_data_preparation/output/2-cleaned_collar_data.csv")
+
+move <- align_move(data, res = 60, unit = "mins")
