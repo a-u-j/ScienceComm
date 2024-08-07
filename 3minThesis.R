6f6c21ebaea16e67f7b62805b5b269e504df1c91
a-u-j <amelia.jeffery@unsw.edu.au>
2024-08-07 10:40:45 +1000

    Added comments for readability

--- 3minThesis.R
+++ 3minThesis.R
@@ -11,16 +11,25 @@ library(dplyr)
 
 data <- read.csv("~/GitHub/collar_data_preparation/output/2-cleaned_collar_data.csv")
 
+# Date needs formatting to POSIXct
 data$formatted_date <- as.POSIXct(data$formatted_date, format="%Y-%m-%d %H:%M", tz="Australia/Sydney")
+
+# Remove NAs in date column
 data <- data[!is.na(data$formatted_date), ]
+
+# Remove duplicated entries for date and ID
 data <- data[!duplicated(data[c("ID", "formatted_date")]), ]
 
-socks <- subset(data, ID == "UOM2008")
+# Select for particular individual
+andy <- subset(data, ID == "UOM2008")
 
-dm <- move(x=socks$Longitude, y=socks$Latitude, time=socks$formatted_date, proj=CRS("+proj=longlat +ellps=WGS84"),animal = socks$ID, removeDuplicatedTimestamps = TRUE)
+# Create move object
+dm <- move(x=andy$Longitude, y=andy$Latitude, time=andy$formatted_date, proj=CRS("+proj=longlat +ellps=WGS84"),animal = andy$ID, removeDuplicatedTimestamps = TRUE)
 
+# Align data to uniform time scale
 move <- moveVis::align_move(dm, res = 60, unit = "mins")
 
+# Create frames
 frames <- moveVis::frames_spatial(move, map_service="mapbox", map_type="satellite", map_token="pk.eyJ1IjoiYXV2YTExNDYiLCJhIjoiY2x6aTI5bzlqMGFxNDJrcHNubWUxMzh2bSJ9.gUe9SUOXO2ozN6J_-KmWDQ") %>%
   moveVis::add_labels(x="Longitude", y="Latitude") %>%
   moveVis::add_northarrow() %>%
@@ -28,6 +37,8 @@ frames <- moveVis::frames_spatial(move, map_service="mapbox", map_type="satellit
   moveVis::add_timestamps(move, type = "label") %>%
   moveVis::add_progress()
 
+# Inspect single frame
 frames[[10]]
 
+# Animate
 moveVis::animate_frames(frames,out_file="moveVis.gif")
